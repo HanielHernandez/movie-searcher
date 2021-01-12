b3aee28b46b113778ead44969685563e8815e418
Haniel <hhernandez@aimservices.tech>
2021-01-12 09:44:16 -0600

    home started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'movies',
+    loadChildren: () => import('./modules/movies/movies.module').then((m) => m.MoviesModule),
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
